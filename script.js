93724d89b00b822b71197de7555df55f0681195d
H4m4k <pawel.janik.1983@gmail.com>
2021-05-02 23:32:09 +0200

    resolved trouble with menu marker placed with offset

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const nav = document.querySelector('#nav');
 function show_Menu() {
     nav.classList.toggle('off')
 }
+
+/* const faq_List = document.querySelectorAll('.list');
+
+faq_List.forEach( list => list.addEventListener('click', toggle))
+
+function toggle (event) {
+    let rotate = event.target.children[0].style.transform || 'rotate(0deg)';
+
+    event.target.classList.toggle('addWeight');
+    // event.target.nextElementSibling.classList.toggle('off');
+    
+    if(rotate === 'rotate(0deg)') return event.target.children[0].style.transform = 'rotate(180deg)'; 
+    return event.target.children[0].style.transform = 'rotate(0deg)'
+} */
